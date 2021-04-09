7ed4652e7919f05d404806fb727a0ac0fe7f7ffb
deniafe <deniafe@yahoo.com>
2021-04-09 13:32:52 +0100

    Trying out social

--- pages/index.vue
+++ pages/index.vue
@@ -12,21 +12,44 @@
         >
           Documentation
         </a>
+
         <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
           class="button--grey"
+          target="_blank"
+          href="http://www.facebook.com/sharer/sharer.php?u=https://csb-cfmxj-g957jytjv.vercel.app/"
+          >Facebook</a
         >
-          GitHub
-        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  head: {
+    titleTemplate: '%s - Nuxt.js',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+      // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+      { hid: 'description', name: 'description', content: 'Meta description' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      { property: 'og:url', content: 'http://devrl.link' },
+      { property: 'og:type', content: 'article' },
+      { property: 'og:title', content: 'Deeviral Page' },
+      {
+        property: 'og:description',
+        content: 'The best Viral marketing platform to use',
+      },
+      {
+        property: 'og:image',
+        content:
+          'https://firebasestorage.googleapis.com/v0/b/telephy-147d6.appspot.com/o/PzJDgebXYOZNbdWE20sOOHhXyYF2%2F342d947fa1e53-A%20whole%20new%20look.jpg?alt=media&token=7b75766f-4786-40b4-bd9e-1c11526e8e32',
+      },
+    ],
+  },
+}
 </script>
 
 <style>
